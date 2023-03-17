c99170234e6d390be736b8c57a0caa1976c3cf1c
Orlando Garcia <orlando.garcia@shopify.com>
2023-03-17 10:18:09 -0400

    new plugins!

--- nvim.config/lua/custom/plugins.lua
+++ nvim.config/lua/custom/plugins.lua
@@ -13,6 +13,24 @@ local function plugins(use)
       vim.g.copilot_no_tab_map = true
     end
   }
+  use 'kdheepak/lazygit.nvim'
+  use 'wsdjeg/vim-fetch'
+  use 'tpope/vim-dispatch'
+  use { 'vim-test/vim-test',
+    config = function()
+      --nmap <silent> <leader>t :TestNearest<CR>
+      vim.keymap.set('n', '<leader>t', ':TestNearest<CR>', { silent = true, desc = 'Run nearest [t]est' })
+      --nmap <silent> <leader>T :TestFile<CR>
+      vim.keymap.set('n', '<leader>T', ':TestFile<CR>', { silent = true, desc = 'Run [T]est file' })
+      --nmap <silent> <leader>a :TestSuite<CR>
+      vim.keymap.set('n', '<leader>a', ':TestSuite<CR>', { silent = true, desc = 'Run [a]ll tests' })
+      --nmap <silent> <leader>l :TestLast<CR>
+      vim.keymap.set('n', '<leader>l', ':TestLast<CR>', { silent = true, desc = 'Run [l]ast test' })
+      --nmap <silent> <leader>g :TestVisit<CR
+      vim.keymap.set('n', '<leader>v', ':TestVisit<CR>', { silent = true, desc = '[v]isit generated test file' })
+      vim.g['test#strategy'] = 'dispatch'
+    end
+  }
 end
 
 return plugins
